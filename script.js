94c16a3d6919dd4a1e969fbb3d49f1e1566591a5
Obinna Umerah <umerahobinna@gmail.com>
2024-03-19 13:04:19 -0500

    Added function and tested out its functionality. Also renamed javascript file

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+console.log(add(1, 2));
+console.log(subtract(5, 2));
+console.log(divide(2, 2));;
