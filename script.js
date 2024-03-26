8e799364361772be93cc88ed232d825dcd034d49
Obinna Umerah <umerahobinna@gmail.com>
2024-03-26 17:56:36 -0500

    Updated logic for calculator

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const digits = document.querySelectorAll(".digit")
+const screen = document.querySelector("#display")
+const operands = document.querySelectorAll("op")
+const solve = document.querySelector("solve")
+const operator = '';
+const firstNumber = 0;
+const secondNumber = 0;
+
+digits.forEach(digit => {
+    digit.addEventListener('click', () => {
+        if(operator === ''){
+        // Store the clicked number into a variable
+        const clickedNumber = digit.innerHTML;
+        // Update the display
+        screen.innerHTML += clickedNumber; // Append the clicked number to the display content
+        // Store the displayed number into a variable
+        firstNumber = screen.innerHTML;
+    } else {
+        // Store the clicked number into a variable
+        const clickedNumber = digit.innerHTML;
+        // Update the display
+        screen.innerHTML += clickedNumber; // Append the clicked number to the display content
+        // Store the displayed number into a variable
+        secondNumber = screen.innerHTML;
+    }
+        });
+    // digit.addEventListener('click', () => {
+    //     // Store the clicked number into a variable
+    //     const clickedNumber = digit.innerHTML;
+    //     // Update the display
+    //     screen.innerHTML += clickedNumber; // Append the clicked number to the display content
+    //     // Store the displayed number into a variable
+    //     firstNumber = screen.innerHTML;
+    // });
+});
+
+operands.forEach(operand => {
+    operand.addEventListener('click', () => {
+        operator = operand.innerHTML;
+        //Call the operate function(Make new event listener for operator)
+
+    })
+    //Another event listener for solving?
+        solve.addEventListener('click', () => {
+            operate(firstNumber, secondNumber, operator);
+        });
+
+})
+
 function add(x, y) {
     return x + y;
 }
@@ -15,9 +64,10 @@ function divide(x, y) {
 }
 
 //Will be used to display numbers later
-let firstNum = 0;
-let operator;
-let secondNum = 0;
+//Already used above
+// let firstNum = 0;
+// let operator;
+// let secondNum = 0;
 
 function operate(x, y, op) {
     switch (op) {
